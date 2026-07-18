d57e244a0db8db4fde4ba2e3921aaa0ce6eb8fc2
Trisha Singh <trishasingh.action@gmail.com>
2026-07-18 12:38:22 -0400

    Stage 5: Swagger UI

--- main.py
+++ main.py
@@ -10,18 +10,30 @@ tasks = [
 
 @app.get("/")
 async def root():
+    """
+    This endpoint returns the basic details of this API
+    """
     return {"name": "Task API", "version" : "1.0", "endpoints" : ["/tasks"]}
 
 @app.get("/health")
 async def getStatus():
+    """
+    This endpoint returns the status of the API. If it returns OK, the API is working. Else, it is not.
+    """
     return{"status": "OK"}
 
 @app.get("/tasks")
 async def getTasks():
+    """
+    This endpoint returns the list of tasks that serve as the local database for Assignment 1.
+    """
     return tasks
 
 @app.get("/tasks/{id}")
 async def getOneTask(id: int):
+    """
+    This endpoint returns only one task depending on the ID in the URL.
+    """
     for i in range(len(tasks)):
         currentTask = tasks[i]
         if(currentTask.get("id") == id):
@@ -34,6 +46,9 @@ async def getOneTask(id: int):
 
 @app.post("/tasks", status_code=201)
 async def makeTask(userTask: dict):
+    """
+    This endpoint makes a task depending on user input.
+    """
     if not userTask.get("title"):
         raise HTTPException(
         status_code=400,
@@ -46,6 +61,9 @@ async def makeTask(userTask: dict):
 
 @app.put("/tasks/{id}", status_code=201)
 async def updateTask(userTask: dict):
+    """
+    This endpoint updates the user's task depending on their input.
+    """
 
     if not userTask:
         raise HTTPException(
@@ -70,6 +88,9 @@ async def updateTask(userTask: dict):
 
 @app.delete("/tasks/{id}", status_code=204)
 async def deleteTask(userTask: dict):
+    """
+    This endpoint deletes the task the user wants deleted
+    """
     if not userTask.get("id"):
         raise HTTPException(
             status_code=404,
